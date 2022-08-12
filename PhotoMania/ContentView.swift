986ca3e307fb50cce239cff8190419180b11b34d
SAS <sas@SASs-Mac.local>
2022-08-12 23:47:09 +0500

    Complete Photo Mania App

---
+++ PhotoMania/ContentView.swift
@@ -0,0 +1,73 @@
+import SwiftUI
+class ViewModel: ObservableObject {
+    @Published var image: Image?
+    func fetchNewImage() {
+        guard let url = URL(string: "https://random.imagecdn.app/500/700")
+        else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) {
+            data, _, _ in
+            guard let data = data
+            else {
+                return
+            }
+            DispatchQueue.main.async {
+                guard let uiImage = UIImage(data: data)
+                else {
+                    return
+                }
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+        task.resume()
+    }
+}
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    var body: some View {
+        NavigationView {
+            VStack {
+                Spacer()
+                if let image = viewModel.image {
+                    ZStack{
+                        image
+                            .resizable()
+                            .foregroundColor(Color.pink)
+                            .frame(width: 350, height: 490)
+                            .padding()
+                    }
+                    .frame(width: 380, height: 520)
+                    .background(Color.pink)
+                    .cornerRadius(8)
+                } else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .foregroundColor(Color.pink)
+                        .frame(width: 200, height: 200)
+                        .padding()
+                }
+                
+                Spacer()
+                Button(action: {
+                    viewModel.fetchNewImage()
+                }, label: {
+                    Text("New Image!")
+                        .bold()
+                        .frame(width: 250, height: 55)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .padding()
+                })
+            }
+            .navigationTitle("Photo Mania")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoMania/PhotoManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoManiaApp.swift
+//  PhotoMania
+//
+//  Created by SAS on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
